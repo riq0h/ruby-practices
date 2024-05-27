da08c1452a718c2053e1b6baa2dc2327e37e7814
Rikuoh <mail@riq0h.jp>
2024-05-27 22:01:57 +0900

    lsコマンドを作る4

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
 
 COLUMNS = 3
+MARGINS = 3
+
+TYPES = {
+  'fifo' => 'p',
+  'characterSpecial' => 'c',
+  'directory' => 'd',
+  'blockSpecial' => 'b',
+  'file' => '-',
+  'link' => 'l',
+  'socket' => 's'
+}.freeze
+
+PERMISSIONS = {
+  '0' => '---',
+  '1' => '--x',
+  '2' => '-w-',
+  '3' => '-wx',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx'
+}.freeze
 
 def run
   listed_filenames = list_filenames
@@ -12,14 +35,66 @@ def run
 end
 
 def list_filenames
-  params = ARGV.getopts('a', 'r')
-  flags = params['a'] ? File::FNM_DOTMATCH : 0
-  filenames = Dir.glob('*', flags)
-  if params['r']
-    filenames.reverse
+  params = ARGV.getopts('a', 'r', 'l')
+  filenames = params['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+  filenames = filenames.reverse if params['r']
+  if params['l']
+    outputs(filenames)
+    exit
   else
     filenames
   end
+end
+
+def outputs(filenames)
+  fileblocks(filenames)
+  filenames.each do |file|
+    file_stat = File::Stat.new(file)
+    print TYPES[file_stat.ftype]
+    filemods(file_stat)
+    prints(file_stat, filenames)
+    timestamps(file_stat)
+    symbolics(file)
+    puts
+  end
+end
+
+def prints(file_stat, filenames)
+  print " #{file_stat.nlink}"
+  print " #{Etc.getpwuid(file_stat.uid).name}"
+  print " #{Etc.getgrgid(file_stat.gid).name}"
+  print " #{file_stat.size}".rjust(filesizes(filenames))
+end
+
+def symbolics(filenames)
+  if File.lstat(filenames).symlink?
+    print " #{filenames} -> #{File.readlink(filenames)}"
+  else
+    print " #{filenames}"
+  end
+end
+
+def filemods(file_stat)
+  file_count = file_stat.mode.to_s(8).slice(-3, 3)
+  file_permission = file_count.split('').map do |file|
+    PERMISSIONS[file]
+  end
+  print file_permission.join('')
+end
+
+def filesizes(filenames)
+  filenames.map { |file| File.size(file) }.max.to_s.length + MARGIN
+end
+
+def timestamps(file_stat)
+  print file_stat.mtime.strftime('%_m月 %_d %H:%M')
+end
+
+def fileblocks(filenames)
+  blocks = filenames.map do |file|
+    File::Stat.new(file).blocks
+  end
+  puts "total #{blocks.sum}"
 end
 
 def slice_filenames(listed_filenames)
