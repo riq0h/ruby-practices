a5080460edb01133c789534b7df6c99bc721d415
Rikuoh <mail@riq0h.jp>
2024-07-21 09:52:49 +0900

    三項演算子と変数名の修正および配列のハッシュ化、ARGFの抑制、メソッドの再配置

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -10,60 +10,16 @@ def parse_options
     opts.on('-c') { options[:bytes] = true }
   end.parse!
   options = { bytes: true, lines: true, words: true } if options.empty?
-  [options, ARGV.empty? ? [ARGF] : ARGV]
+  sources = ARGV.empty? ? ['-'] : ARGV
+  [options, sources]
 end
 
-def count_file_stats(input)
-  lines = words = bytes = 0
-  input.each_line do |line|
-    lines += 1
-    words += line.split.size
-    bytes += line.bytesize
-  end
-  [lines, words, bytes]
-end
-
-def process_input(source)
-  if source == ARGF || File.exist?(source)
-    input = source == ARGF ? ARGF.read : File.read(source)
-    count_file_stats(input)
-  else
-    puts "wc: #{source}: そのようなファイルやディレクトリはありません"
-    exit
-  end
-end
-
-def update_totals(totals, stats)
-  totals.zip(stats).map { |total, stat| total + stat }
-end
-
-def update_max_widths(max_widths, stats)
-  max_widths.zip(stats).map { |max, stat| [max, stat.to_s.length].max }
-end
-
-def format_result(stats, max_widths, options)
-  result = []
-  %i[lines words bytes].each_with_index do |key, index|
-    result << stats[index].to_s.rjust(max_widths[index]) if options[key]
-  end
-  result.join(' ')
-end
-
-def print_result(result, source)
-  puts "#{result} #{source == ARGF ? '' : source}"
-end
-
-def print_total(total_stats, max_widths, options)
-  total_result = format_result(total_stats, max_widths, options)
-  puts "#{total_result} 合計"
-end
-
-def process_input_sources(input_sources, options)
-  total_stats = [0, 0, 0]
-  max_widths = [0, 0, 0]
+def input_stream_sources(input_sources, options)
+  total_stats = { lines: 0, words: 0, bytes: 0 }
+  max_widths = { lines: 0, words: 0, bytes: 0 }
 
   input_sources.each do |source|
-    stats = process_input(source)
+    stats = input_stream(source)
     total_stats = update_totals(total_stats, stats)
     max_widths = update_max_widths(max_widths, stats)
     result = format_result(stats, max_widths, options)
@@ -73,5 +29,48 @@ def process_input_sources(input_sources, options)
   print_total(total_stats, max_widths, options) if input_sources.size > 1
 end
 
+def input_stream(source)
+  if source == '-'
+    count_file_stats(ARGF)
+  else
+    count_file_stats(File.read(source))
+  end
+end
+
+def count_file_stats(input)
+  stats = { lines: 0, words: 0, bytes: 0 }
+  input.each_line do |line|
+    stats[:lines] += 1
+    stats[:words] += line.split.size
+    stats[:bytes] += line.bytesize
+  end
+  stats
+end
+
+def update_totals(totals, stats)
+  totals.merge(stats) { |_key, total, stat| total + stat }
+end
+
+def update_max_widths(max_widths, stats)
+  max_widths.merge(stats) { |_key, max, stat| [max, stat.to_s.length].max }
+end
+
+def format_result(stats, max_widths, options)
+  result = {}
+  %i[lines words bytes].each do |key|
+    result[key] = stats[key].to_s.rjust(max_widths[key]) if options[key]
+  end
+  result.values.join(' ')
+end
+
+def print_result(result, source)
+  puts "#{result} #{source == '-' ? '' : source}"
+end
+
+def print_total(total_stats, max_widths, options)
+  total_result = format_result(total_stats, max_widths, options)
+  puts "#{total_result} 合計"
+end
+
 options, input_sources = parse_options
-process_input_sources(input_sources, options)
+input_stream_sources(input_sources, options)
