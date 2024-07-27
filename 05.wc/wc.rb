1d7a649edbde3351fc088d391f6c0c73ec42da03
Rikuoh <mail@riq0h.jp>
2024-07-27 16:14:18 +0900

    カウント情報とファイル名のハッシュ化、文字幅のロジックを変更、filter_mapの利用

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -8,7 +8,7 @@ def main
   total_stats = calculate_total_stats(file_stats)
   max_widths = calculate_max_widths(file_stats, total_stats)
   print_file_stats(file_stats, max_widths, options)
-  print_file_stats([['合計', total_stats]], max_widths, options) if sources.size > 1
+  print_file_stats([{ filename: '合計', **total_stats }], max_widths, options) if sources.size > 1
 end
 
 def parse_options
@@ -26,31 +26,37 @@ end
 def collect_file_stats(sources)
   sources.map do |source|
     input = source.empty? ? ARGF.read : File.read(source)
-    stats = { lines: input.lines.count, words: input.split.size, bytes: input.bytesize }
-    [source, stats]
+    { filename: source, lines: input.lines.count, words: input.split.size, bytes: input.bytesize }
   end
 end
 
 def calculate_total_stats(file_stats)
-  file_stats.reduce({ lines: 0, words: 0, bytes: 0 }) do |total, (_, stats)|
-    total.merge(stats) { |_, a, b| a + b }
+  total_stats = { lines: 0, words: 0, bytes: 0 }
+  file_stats.each do |stats|
+    total_stats[:lines] += stats[:lines]
+    total_stats[:words] += stats[:words]
+    total_stats[:bytes] += stats[:bytes]
   end
+  total_stats
 end
 
 def calculate_max_widths(file_stats, total_stats)
-  (file_stats.map(&:last) + [total_stats]).each_with_object({ lines: 0, words: 0, bytes: 0 }) do |stats, max_widths|
-    max_widths.merge!(stats) { |_, max, stat| [max, stat.to_s.length].max }
+  all_stats = file_stats + [total_stats]
+  %i[lines words bytes].each_with_object({}) do |key, max_widths|
+    max_widths[key] = all_stats.map { |stats| stats[key].to_s.length }.max
   end
 end
 
 def format_result(stats, max_widths, options)
-  %i[lines words bytes].map { |key| stats[key].to_s.rjust(max_widths[key]) if options[key] }.compact.join(' ')
+  %i[lines words bytes].filter_map do |key|
+    stats[key].to_s.rjust(max_widths[key]) if options[key]
+  end.join(' ')
 end
 
 def print_file_stats(file_stats, max_widths, options)
-  file_stats.each do |source, stats|
+  file_stats.each do |stats|
     result = format_result(stats, max_widths, options)
-    puts "#{result} #{source}"
+    puts "#{result} #{stats[:filename]}"
   end
 end
 
